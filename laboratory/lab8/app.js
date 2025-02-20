b0f8118616000789099850d043ecf374e327a371
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-02-20 12:44:19 -0600

    ADD: example of asynchronic code

--- laboratory/lab8/app.js
+++ laboratory/lab8/app.js
@@ -4,3 +4,11 @@ const file_system = require('fs');
 // write file system para escribir codigo asincronico (bloquea todo que pasa en la computadora)
 // el sincrono
 file_system.writeFileSync('hola.txt', 'Hola desde node');
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
