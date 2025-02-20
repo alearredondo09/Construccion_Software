16eb4317c17f78806ba111826b8a5aafccc71804
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-02-20 14:12:44 -0600

    ADD: function string in txt. Done by Ale and Kami

--- laboratory/lab8/lab08.js
+++ laboratory/lab8/lab08.js
@@ -1,13 +1,25 @@
 
 // Una función que reciba un arreglo de números y devuelva su promedio.
 const arreglo = [30, 28, 983, 278, 390, 267, 489, 398, 1289];
-let promedio = 0;
-let arr_lon = arreglo.length;
+function cal_promedio(arreglo){
+    let promedio = 0;
+    let arr_lon = arreglo.length;
+    
+    for (let i = 0; i < arreglo.length; i++){
+        promedio += arreglo[i];
+        // console.log(promedio );
+    };
 
-for (let i = 0; i < arreglo.length; i++){
-    promedio += arreglo[i];
-    console.log(promedio );
+    promedio = promedio / arr_lon;
+    console.log(`El promedio del arreglo es: ${promedio.toFixed(2)}`);
+}
+
+// Una función que reciba un string y escriba el string en un archivo de texto. Apóyate del módulo fs.
+const texto = 'Laboratorio de Lalo LOL';
+function imprimir_string(texto){
+    const file_system = require('fs');
+    file_system.writeFileSync('string.txt', texto);
 };
-console.log(promedio = promedio / arr_lon);
 
-console.log(promedio.toFixed(2));
+cal_promedio(arreglo);
+imprimir_string(texto);
