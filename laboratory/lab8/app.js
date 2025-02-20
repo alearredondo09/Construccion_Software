1a4f1392f29a6e8d1a6ed56cf6195e959a3e9de7
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-02-20 12:58:04 -0600

    ADD: create a server with 3 lines of code

--- laboratory/lab8/app.js
+++ laboratory/lab8/app.js
@@ -1,20 +1,32 @@
-// busca el módulo de fs que es el modulo del file system
-const file_system = require('fs');
+// // busca el módulo de fs que es el modulo del file system
+// const file_system = require('fs');
 
-// write file system para escribir codigo asincronico (bloquea todo que pasa en la computadora)
-// el sincrono
-file_system.writeFileSync('hola.txt', 'Hola desde node');
+// // write file system para escribir codigo asincronico (bloquea todo que pasa en la computadora)
+// // el sincrono
+// file_system.writeFileSync('hola.txt', 'Hola desde node');
 
-// Codigo asincrono no va en secuenncia, esto se imprime de acorde al tiempo en el que le dices que se imprime a comparación de los valores que 
-// tiene el arreglo 
-setTimeout(() => {console.log('jojo')}, 20000);
-setTimeout(() => {console.log('jojo')}, 9000);
+// // Codigo asincrono no va en secuenncia, esto se imprime de acorde al tiempo en el que le dices que se imprime a comparación de los valores que 
+// // tiene el arreglo 
+// setTimeout(() => {console.log('jojo')}, 20000);
+// setTimeout(() => {console.log('jojo')}, 9000);
 
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+// const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
-for (let item of arreglo){
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-}
+// for (let item of arreglo){
+//     setTimeout(() => {
+//         console.log(item);
+//     }, item);
+// }
 
+const http = require('http');
+
+//objeto request => tiene la info de la petición http
+// objeto response => tiene toda la info de la información que se enviará devuelta
+// 
+const server = http.createServer( (request, response) => {
+    // console.log(request.url);
+    // 
+});
+
+// el servidor esta escuchando por el puerto 3000 (puedes ponerle cualquier numero arriba del 3000)
+server.listen(3000);
