7501e1717cc52f2f2c331d293ce2b0430c8eba69
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-02-27 13:25:34 -0600

    ADD: dynamic HTML and pass variables through templates

--- laboratory/labs/routes/plantas.routes.js
+++ laboratory/labs/routes/plantas.routes.js
@@ -1,41 +1,11 @@
 const express = require('express');
 const router = express.Router();
-            
-const html_form = `<form action="/plantas/agregar" method="POST">
-              <label for="nombre" class="label">Nombre de la planta</label>
-              <input
-                class="input is-info"
-                type="text"
-                placeholder="Orquídea"
-                id="nombre"
-                name="nombre"
-              />
-              <br><br>
-              <input class="button is-info" type="submit" value="Enviar">
-            </form>`;
-const html_footer = `</div>
-    </section>
-    <footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
-            The source code is licensed
-            <a href="https://opensource.org/license/mit">MIT</a>. The
-            website content is licensed
-            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
-              >CC BY NC SA 4.0</a
-            >.
-          </p>
-        </div>
-      </footer>
-      <script src="js/introjs.js"></script>
-  </body>
-</html>
-`;
+const html_header = ``;
+const html_footer = ``;
 const plantas = [];
 //router.get es para registrar un middleware para peticiones HTTP GET
 router.get('/agregar', (request, response, next) => {
-    response.send(html_header + html_form + html_footer);
+    response.render('agregar_planta');
 });
 //router.post es para registrar un middleware para peticiones HTTP POST
 router.post('/agregar', (request, response, next) => {
@@ -57,6 +27,9 @@ router.post('/agregar', (request, response, next) => {
     html += `</div>`;
     html += html_footer;
     response.send(html);
+    response.render('lista_plantas', {
+      plantas: plantas,
+    });
 });
 const path = require('path');
 router.get('/regar', (request, response, next) => {
