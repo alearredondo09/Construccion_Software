d02e127b3ad67e81272614c40b65c37fa2ad3034
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-03-04 13:12:07 -0600

    ADD: cookies added

--- laboratory/practice_Lalo/controllers/users.controller.js
+++ laboratory/practice_Lalo/controllers/users.controller.js
@@ -1,7 +1,8 @@
 exports.get_login = (request, response, next) => {
     response.render('login.ejs',{
         // si la variable isLoggedIn tiene un valor indefinido le va a dar un valor falso para evitar que todo truene horriblemente
-        isLoggedIn : request.session.isLoggedIn || false
+        isLoggedIn : request.session.isLoggedIn || false,
+        username: request.session.username || '',
     });
 };
 
@@ -9,4 +10,10 @@ exports.post_login = (request, response, next) => {
     request.session.isLoggedIn = true; // varible para ver si está logeado
     request.session.username = request.body.username;
     response.redirect('/plantas');
+};
+
+exports.get_logout = (request, response, next) => {
+    request.session.destroy(() => {
+        response.redirect('/users/login'); //mata la sesion y redirige a la main page
+    })
 };
